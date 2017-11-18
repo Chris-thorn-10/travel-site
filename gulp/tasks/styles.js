066b896cd2bcaa3d7f834c2cbd1eda413da104e5
Christie <thornton.christie@gmail.com>
2017-11-18 18:39:04 -0500

    Updated header.

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -8,7 +8,7 @@ var mixins = require('postcss-mixins');
 var hexrgba = require('postcss-hexrgba');
 
 
-gulp.task('styles', function(){
+gulp.task('styles', function() {
 	return gulp.src('./app/assets/styles/styles.css')
 		.pipe(postcss([cssImport, mixins, cssvars, nested, hexrgba, autoprefixer]))
 		.on('error',function(errorInfo){
--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,3 @@ require('./gulp/tasks/styles');	//.js not necessary
 require('./gulp/tasks/watch');	//.js not necessary
 require('./gulp/tasks/sprites');	//.js not necessary
 require('./gulp/tasks/scripts');	//.js not necessary
-
-
-
-
-
-
-
