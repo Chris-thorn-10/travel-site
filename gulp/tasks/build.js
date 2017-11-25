e2c5e17fcd84c964211bb70c462756545e33ccaa
Christie <thornton.christie@gmail.com>
2017-11-24 20:58:10 -0500

    Completed gulp build task.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -2,11 +2,24 @@ var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var del = require('del');
 var usemin = require('gulp-usemin');
+var rev = require('gulp-rev');
+var cssnano = require('gulp-cssnano');
+var uglify = require('gulp-uglify');
+var browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function() {
+    browserSync.init({
+		notify:false,
+		server:{
+			baseDir: "docs"	
+			}
+		});		
+});
 
 
 //delete the dist folder every time we run the build task
-gulp.task('deleteDistFolder', function() {
-    return del('./dist');
+gulp.task('deleteDistFolder', ['icons'], function() {
+    return del('./docs');
 });
 
 //copy and compress image files
@@ -17,17 +30,23 @@ gulp.task('optimizeImages',['deleteDistFolder'], function() {
             interlaced: true,
             multipass: true,
     }))
-        .pipe(gulp.dest('./dist/assets/images'));
+        .pipe(gulp.dest('./docs/assets/images'));
 });
 
+gulp.task('useminTrigger', ['deleteDistFolder'], function() {
+   gulp.start("usemin"); 
+});
 
 //copy, compress, and revision (add letters and numbers to break caching)css and java generated files (none of the modules)
-gulp.task('usemin',['deleteDistFolder'], function(){
+gulp.task('usemin',['styles', 'scripts'], function(){
     return gulp.src('./app/index.html')
-        .pipe(usemin())
-        .pipe(gulp.dest('./dist'));
+        .pipe(usemin({
+        css: [function() {return rev()}, function() {return cssnano()}],
+        js: [function() {return rev()}, function() {return uglify()}]
+    }))
+        .pipe(gulp.dest('./docs'));
 });
 
 
-gulp.task('build', ['deleteDistFolder','optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDistFolder','optimizeImages', 'useminTrigger']);
 
